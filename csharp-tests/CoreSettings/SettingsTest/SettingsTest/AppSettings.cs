35b9625aa65bd5b7a5cf3504f4d2f44aabb4f136
Adilson de Almeida Junior <adilson.smem@gmail.com>
2018-04-20 21:11:06 -0300

    Teste config

---
+++ csharp-tests/CoreSettings/SettingsTest/SettingsTest/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SettingsTest
+{
+    public class AppSettings
+    {
+        public string Environment { get; set; }
+        public Dictionary<string,string> StorageAccounts { get; set; }
+        public bool FeatureEnabled { get; set; }
+    }
+}
---
+++ csharp-tests/CoreSettings/SettingsTest/SettingsTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace SettingsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfigurationRoot config;
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("environmentsettings.json");
+
+            config = configurationBuilder.Build();
+            var settings = new AppSettings();
+            config.Bind("appsettings", settings);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
